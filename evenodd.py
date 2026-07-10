59e47dfd382b34d452378ef6b288b7003deb0f5f
Kedarkd12 <kedar.antarkar@gmail.com>
2026-07-10 16:42:18 +0530

    Added comments

--- evenodd.py
+++ evenodd.py
@@ -1,7 +1,7 @@
 #Ask user for a number
 a=int(input("Enter a number:"))
 
-#check whether given number is even or odd
+#check whether given number is even or odd and print accordingly
 if(a%2==0):
     print("Even")
 else:
