8cef69a318873c6655380da8d5843cde90f5954c
Kedarkd12 <kedar.antarkar@gmail.com>
2026-07-11 17:56:57 +0530

    Function created

--- tableofnumber.py
+++ tableofnumber.py
@@ -1,20 +1,26 @@
-#Ask user for a number
-num=int(input("Enter a number:"))
+def tables(num):
+    if num>0:
+        #print table of positive integers
+        i=1
+        print("Table of",num,"using while loop:")
+        while i<=10:
+            c=num*i
+            print(num,"*",i,"=",c)
+            i=i+1
+    #print table of negative integers
+    else:
+        print("Table of",num,"using for loop:")
+        for j in range(1,10):
+            j<=10
+            d=num*j
+            print(num,"*",j,"=",d)
+            j+=1
 
-if num>0:
-    #print table of positive integers
-    i=1
-    print("Table of",num,"using while loop:")
-    while i<=10:
-        c=num*i
-        print(num,"*",i,"=",c)
-        i=i+1
+def main():
+    #Ask user for a number
+    num=int(input("Enter a number:"))
+    tables(num)
 
-#print table of negative integers
-else:
-    print("Table of",num,"using for loop:")
-    for j in range(1,10):
-        j<=10
-        d=num*j
-        print(num,"*",j,"=",d)
-        j+=1
+main()
+
+
