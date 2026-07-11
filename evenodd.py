9c701b6cc331e8790a31816de70b05b8b7999751
Kedarkd12 <kedar.antarkar@gmail.com>
2026-07-11 17:55:50 +0530

    Function implemented

--- evenodd.py
+++ evenodd.py
@@ -1,8 +1,13 @@
-#Ask user for a number
-a=int(input("Enter a number:"))
+def parity(a):
+    #check whether given number is even or odd and print accordingly
+    if(a%2==0):
+        print("Even")
+    else:
+        print("Odd")
 
-#check whether given number is even or odd and print accordingly
-if(a%2==0):
-    print("Even")
-else:
-    print("Odd")
+def main():
+    #Ask user for a number
+    b=int(input("Enter a number:"))
+    parity(b)
+
+main()
