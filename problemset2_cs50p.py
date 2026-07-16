8b9d75c8dbb43891e7bc82b9cc92441f2dd868d2
Kedarkd12 <kedar.antarkar@gmail.com>
2026-07-16 21:22:53 +0530

    cs50p Problem set 2 solved

--- problemset2_cs50p.py
+++ problemset2_cs50p.py
@@ -1,88 +1,88 @@
 # #camelCase
-# word=input("camelCase: ")
-# print("snake_case: ",end="")
-# for letter in word:
-#     if letter.isupper():
-#         print("_"+letter.lower(),end="")
-#     else:
-#         print(letter,end="")
-# print()
+word=input("camelCase: ")
+print("snake_case: ",end="")
+for letter in word:
+    if letter.isupper():
+        print("_"+letter.lower(),end="")
+    else:
+        print(letter,end="")
+print()
 
 # #coke machine
-# amount=50
-# coin=0
-# while amount>0:
-#     print("Amount Due:",amount)
-#     coin=int(input("Enter Coin:"))
-#     match coin:
-#         case 25:
-#             amount-=25
-#         case 10:
-#             amount-=10
-#         case 5:
-#             amount-=5
-#         case _:
-#             continue
-# if(amount==0):
-#     print("Change Owed:",amount)
-# else:
-#     print("Change Owed:",abs(amount))
+amount=50
+coin=0
+while amount>0:
+    print("Amount Due:",amount)
+    coin=int(input("Enter Coin:"))
+    match coin:
+        case 25:
+            amount-=25
+        case 10:
+            amount-=10
+        case 5:
+            amount-=5
+        case _:
+            continue
+if(amount==0):
+    print("Change Owed:",amount)
+else:
+    print("Change Owed:",abs(amount))
 
 #just setting up my twttr
-# k=input("Input:")
-# print("Output:",end="")
-# for j in k:
-#     match j:
-#         case "a" | "A":
-#             j.replace("a","").replace("A","")
-#         case "e" | "E":
-#             j.replace("e","").replace("E","")
-#         case "i" | "I":
-#             j.replace("i","").replace("I","")
-#         case "o" | "O":
-#             j.replace("o","").replace("O","")
-#         case "u" | "U":
-#             j.replace("u","").replace("U","")
-#         case _:
-#             print(j,end="")
+k=input("Input:")
+print("Output:",end="")
+for j in k:
+    match j:
+        case "a" | "A":
+            j.replace("a","").replace("A","")
+        case "e" | "E":
+            j.replace("e","").replace("E","")
+        case "i" | "I":
+            j.replace("i","").replace("I","")
+        case "o" | "O":
+            j.replace("o","").replace("O","")
+        case "u" | "U":
+            j.replace("u","").replace("U","")
+        case _:
+            print(j,end="")
 
 #vanity plate
-# def main():
-#     plate = input("Plate: ")
-#     if is_valid(plate):
-#         print("Valid")
-#     else:
-#         print("Invalid")
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
-# def is_valid(s):
-#     n=len(s)
-#     if n<=6 and n>=2:
-#         if s.isalnum():
-#             if s[0].isalpha() and s[1].isalpha():
-#                 if digs(s):
-#                    if chars(s):
-#                        return True 
+def is_valid(s):
+    n=len(s)
+    if n<=6 and n>=2:
+        if s.isalnum():
+            if s[0].isalpha() and s[1].isalpha():
+                if digs(s):
+                   if chars(s):
+                       return True 
 
-# def digs(s):
-#     for char in s:
-#         if char.isdigit():
-#             if char!="0":
-#                 return True
-#             else:
-#                 return False
-#         else:
-#             return True
+def digs(s):
+    for char in s:
+        if char.isdigit():
+            if char!="0":
+                return True
+            else:
+                return False
+        else:
+            return True
             
-# def chars(s):
-#     number_started=False
-#     for char in s:
-#         if char.isdigit():
-#             number_started=True
-#         elif char.isalpha() and number_started:
-#             return False
-#         return True
+def chars(s):
+    number_started=False
+    for char in s:
+        if char.isdigit():
+            number_started=True
+        elif char.isalpha() and number_started:
+            return False
+        return True
         
-# main()
+main()
 
 #nutrition table
 fruits=[
